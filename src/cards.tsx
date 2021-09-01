824ad1f240e91e0f3092f69d7ebf1548586bc3fe
Insamedkv <dab.sky1997@gmail.com>
2021-09-01 12:58:41 +0300

    add: webpack-config filles

---
+++ src/cards.tsx
@@ -0,0 +1,30 @@
+export const cards = [
+  {
+    picture: 'cards-images/campfire.png',
+    name: 'Campfire',
+    author: 'Anton Pavlov',
+    likes: 250,
+    views: 512,
+  },
+  {
+    picture: 'cards-images/coconut.png',
+    name: 'Coconut',
+    author: 'Pavel Hayami',
+    likes: 564,
+    views: 798,
+  },
+  {
+    picture: 'cards-images/man.png',
+    name: 'Man',
+    author: 'Li Brodskiy',
+    likes: 948,
+    views: 1000,
+  },
+  {
+    picture: 'cards-images/skull.png',
+    name: 'Skull',
+    author: 'George Mikael',
+    likes: 12,
+    views: 589,
+  },
+];
